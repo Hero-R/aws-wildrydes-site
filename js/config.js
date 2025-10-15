6b38ffa7c967e18575dcc80eb425c048a8b73f3f
Hero-R <63105426+Hero-R@users.noreply.github.com>
2025-10-15 19:46:01 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_OUTwKR9Mz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7dl5s145appmej0qrjpio58olg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '69f2o8lqp78a1606r25nmcf9jv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
